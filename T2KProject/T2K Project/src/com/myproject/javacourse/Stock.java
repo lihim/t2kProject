15f5338954e84fae357f42e2b94fe44c428d5764
lihim <martin.lihi@gmail.com>
2015-01-05 10:47:34 +0200

    1

---
+++ T2KProject/T2K Project/src/com/myproject/javacourse/Stock.java
@@ -0,0 +1,61 @@
+package com.myproject.javacourse;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Stock {
+
+	private String _symbol;
+	private float _ask;
+	private float _bid;
+	private Date date;
+	
+	public Stock(String symbol, double ask, double bid, Date date){
+		this._ask = (float) ask;
+		this._bid = (float) bid;
+		this._symbol = symbol;
+		this.date = date;
+	}
+
+	public String getHtmlDescription(){
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+		dateFormat.format(getDate());
+		
+		String stockHtmlDetailsString;
+
+		stockHtmlDetailsString = "<b>Stock symbol</b>: " +get_symbol()+ " <b>Bid</b> "
+				+get_bid() + " <b>ask</b> " + get_ask() + " <b>date</b> "+ dateFormat.format(getDate());;
+				
+		return stockHtmlDetailsString;
+	}
+	
+
+
+	public String get_symbol() {
+		return _symbol;
+	}
+	public void set_symbol(String _symbol) {
+		this._symbol = _symbol;
+	}
+	public float get_ask() {
+		return _ask;
+	}
+	public void set_ask(float _ask) {
+		this._ask = _ask;
+	}
+	public float get_bid () {
+		return _bid;
+	}
+	public void set_bid(float _bid) {
+		this._bid = _bid;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+}
---
+++ T2KProject/T2K Project/src/com/myproject/javacourse/StockDetailsServlet.java
@@ -0,0 +1,34 @@
+package com.myproject.javacourse;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class StockDetailsServlet  extends HttpServlet{
+
+	@SuppressWarnings("deprecation")
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2014, Calendar.NOVEMBER, 15);
+		
+		Stock PIH = new Stock("PIH", 13.1, 12.4, calendar.getTime());
+		Stock AAL = new Stock("AAL", 5.78, 5.5, calendar.getTime());
+		Stock CAAS = new Stock("CAAS", 32.2, 31.5, calendar.getTime());
+		String resultStr;
+		
+		resultStr = PIH.getHtmlDescription() + "<br>" + AAL.getHtmlDescription() + "<br>" + CAAS.getHtmlDescription();
+		
+		resp.getWriter().println(resultStr);
+	
+	}
+	
+	
+	
+}
