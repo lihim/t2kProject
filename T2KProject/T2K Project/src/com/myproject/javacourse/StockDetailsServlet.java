c2dbac10e26fbe9e646fef7921f08d690ebab8a6
lihim <martin.lihi@gmail.com>
2015-01-06 16:23:23 +0200

    test

--- T2KProject/T2K Project/src/com/myproject/javacourse/StockDetailsServlet.java
+++ T2KProject/T2K Project/src/com/myproject/javacourse/StockDetailsServlet.java
@@ -17,7 +17,7 @@ public class StockDetailsServlet  extends HttpServlet{
 		Calendar calendar = Calendar.getInstance();
 		calendar.set(2014, Calendar.NOVEMBER, 15);
 	
-		
+		//lilili
 		Stock PIH = new Stock("PIH", 13.1f, 12.4f, calendar.getTime());
 		Stock AAL = new Stock("AAL", 5.78f, 5.5f, calendar.getTime());
 		Stock CAAS = new Stock("CAAS", 32.2f, 31.5f, calendar.getTime());
